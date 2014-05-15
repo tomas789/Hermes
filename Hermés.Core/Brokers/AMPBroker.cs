1881ffb6d3b1dec88c707980df5374eec230fa24
Tomas Krejci <tomas789@gmail.com>
2014-05-15 23:49:08 +0200

    simple broker implementation

---
+++ Hermés.Core/Brokers/AMPBroker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermés.Core.Common;
+using Hermés.Core.Events;
+
+namespace Hermés.Core.Brokers
+{
+    class AMPBroker : IBroker
+    {
+        private Kernel _kernel = null;
+
+        public double DefaultCost = 2;
+        public readonly Dictionary<Ticker, double> TradeCosts = 
+            new Dictionary<Ticker, double>(); 
+
+        public void Initialize(Kernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public void DispatchEvent(Event e)
+        {
+            var ts = new TypeSwitch()
+                .Case((OrderEvent x) => DispatchConcrete(x));
+
+            ts.Switch(e);
+        }
+
+        private void DispatchConcrete(OrderEvent ev)
+        {
+            if (_kernel == null)
+                throw new ArgumentNullException();
+
+            var cost = DefaultCost;
+            if (TradeCosts.ContainsKey(ev.Ticker))
+                cost = TradeCosts[ev.Ticker];
+
+            var fill = new FillEvent(ev.Ticker, ev.Direction, ev.Price, ev.Price, cost);
+            _kernel.AddEvent(fill);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
