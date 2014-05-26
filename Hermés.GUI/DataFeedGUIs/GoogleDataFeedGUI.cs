d1b7e839bcd4c7dfbe648afd62078e892e93879b
meciarm <martinmeciar11@gmail.com>
2014-05-26 19:35:20 +0200

    try hard to work on datafeedgui

---
+++ Hermés.GUI/DataFeedGUIs/GoogleDataFeedGUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hermés.GUI;
+using Hermés.Core;
+using Hermés.Core.DataFeeds;
+
+namespace Hermés.GUI.DataFeedGUIs
+{
+    class GoogleDataFeedGUI
+    {
+    }
+}
